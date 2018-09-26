2db405e55b31c3541b905d8799915a0d34dfd1bd
rjurado <rjurado@nosolosoftware.es>
2018-09-26 14:43:55 +0200

    Fixed bug with ActiveRecord::Relation

--- lib/as_json_representations/collection.rb
+++ lib/as_json_representations/collection.rb
@@ -1,13 +1,19 @@
 module AsJsonRepresentations
   module Collection
-    def as_json(options={})
-      map do |item|
-        item.respond_to?(:as_json) ? item.as_json(options) : item
-      end
-    end
-
     def representation(name, options={})
       as_json(options.merge(representation: name))
+    end
+
+    def self.included(base)
+      return if base.respond_to? :as_json
+
+      base.class_eval do
+        def as_json(options={})
+          map do |item|
+            item.respond_to?(:as_json) ? item.as_json(options) : item
+          end
+        end
+      end
     end
   end
 end
